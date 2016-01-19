b5dbd68e39acfef791c567704bb2934ff4459699
Ri Zhao <bill.zhao24@gmail.com>
2016-01-19 17:14:21 -0500

    Initial Commit

---
+++ PartyShark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PartyShark
+//
+//  Created by Ri Zhao on 2016-01-19.
+//  Copyright © 2016 Ri Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
